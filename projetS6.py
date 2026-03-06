84c1ecd90bd740f133a609450f1fcd463009da7f
djimmyy <djemai.anis10@gmail.com>
2026-03-06 23:06:00 +0100

    Update projetS6.py

--- projetS6.py
+++ projetS6.py
@@ -9,10 +9,6 @@ import csv
 import pandas as pd
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
 
 
 
